1d12c95b4efcf7c4f29cdc81c4c75774e63bd214
Juan Intriago <juanintriagovillarreal@hotmail.com>
2022-03-18 00:08:03 -0500

    iniital passed repo

--- src/components/images/constants.tsx
+++ src/components/images/constants.tsx
@@ -1,7 +1,7 @@
-export const pathGirlTyping = 'assets/girlTyping.png';
-export const pathGroupStudying = 'assets/groupStudying.png';
-export const pathGirlSmilling = 'assets/girlSmiling.png';
-export const pathBussinessWoman = 'assets/businessWoman.jpg';
+export const pathGirlTyping = 'assets/girltyping.png';
+export const pathGroupStudying = 'assets/groupstudying.png';
+export const pathGirlSmilling = 'assets/girlsmiling.png';
+export const pathBussinessWoman = 'assets/businesswoman.jpg';
 export const pathCorner = 'assets/corner.png';
 export const pathLogo = 'assets/logo.png';
-export const pathLogoInvert = 'assets/logoInvert.png';
+export const pathLogoInvert = 'assets/logoinvert.png';
